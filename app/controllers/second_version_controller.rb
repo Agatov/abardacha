b9e631a14f09baa1726b8b51076d4358450915b2
agatov <agatovs@gmail.com>
2013-08-06 14:02:13 +0400

    fix

--- app/controllers/second_version_controller.rb
+++ app/controllers/second_version_controller.rb
@@ -3,6 +3,14 @@ class SecondVersionController < ApplicationController
   layout 'second'
 
   def index
+    if params[:r]
+      section = params[:r]
+    else
+      section = "stranica_zahvata"
+    end
+
+    @first_word = I18n.t "titles.#{section}.first"
+    @last_word = I18n.t "titles.#{section}.last"
 
   end
 end
