9fe7cc5a53fd94c8ef710408d12a18748c470e04
agatov <agatovs@gmail.com>
2014-01-16 12:21:45 +0400

    new changes

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,23 +5,40 @@ class Order < ActiveRecord::Base
   after_create :send_to_crm
 
   def send_notification
-    response = HTTParty.get(
-        'http://api.sms24x7.ru',
-        query: {
-            method: 'push_msg',
-            email: 'agatovs@gmail.com',
-            password: 'avv6rqE',
-            phone: '79037928959',
-            text: sms_text,
-            sender_name: 'abardacha'
-        }
-    )
+    #response = HTTParty.get(
+    #    'http://api.sms24x7.ru',
+    #    query: {
+    #        method: 'push_msg',
+    #        email: 'agatovs@gmail.com',
+    #        password: 'avv6rqE',
+    #        phone: '79037928959',
+    #        text: sms_text,
+    #        sender_name: 'abardacha'
+    #    }
+    #)
+    #
+    #if response.parsed_response['response']['msg']['text'] == 'OK'
+    #  true
+    #else
+    #  false
+    #end
 
-    if response.parsed_response['response']['msg']['text'] == 'OK'
-      true
-    else
-      false
-    end
+    Pony.mail ({
+      to: 'abardacha@gmail.com',
+      subject: I18n.t('email.title', locale: 'en'),
+      body: sms_text,
+      via: :smtp,
+      via_options: {
+        address: 'smtp.gmail.com',
+        port: 587,
+        enable_starttls_auto: true,
+        user_name: 'abardacha',
+        password: 'fiolent149',
+        authentication: :plain
+      }
+    })
+
+
   end
 
   def send_to_crm
